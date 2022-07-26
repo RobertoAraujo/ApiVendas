6f540f20ffced95f5c025e68e03deed7f847f4f8
Roberto Araujo <endriosrobert@gmail.com>
2022-07-26 10:55:13 -0500

    first commit

---
+++ src/main/java/com/poshyweb/vendas/config/DBConfig.java
@@ -0,0 +1,21 @@
+package com.poshyweb.vendas.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("homolog")
+public class DBConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DBConfig.class);
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+        LOGGER.info("Rodando a configuração de desenvolvimento");
+        };
+    }
+}
